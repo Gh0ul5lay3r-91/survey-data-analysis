a991b814cbb784946308b8de92e02f5b35dd79ee
Gh0ul5lay3r-91 <brennan.eoin91@gmail.com>
2021-09-30 12:26:00 +0000

    Add code to access google sheet

--- run.py
+++ run.py
@@ -12,7 +12,6 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Survey Data')
 
-sheet1 = SHEET.worksheet('sheet1')
-headings = sheet1.get_all_values()
+headings = SHEET.worksheet[sheet1].get_all_values()[0]
 
 print(headings)
